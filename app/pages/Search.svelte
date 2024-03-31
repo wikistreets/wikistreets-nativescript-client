5e60bbb7fe819322a06a86e83b39b65c56ff9576
rubykiim <sk7383@nyu.edu>
2024-03-31 16:21:04 +0900

    moved searchBar into actionBar

--- app/pages/Search.svelte
+++ app/pages/Search.svelte
@@ -6,23 +6,26 @@ import { l, lc, lt } from '~/services/localeService'
 </script>
   
   <page actionBarHidden={false} class="w-full h-full" {...$$restProps}>
-    <actionBar title="Search" flat="true">
-        <flexboxLayout class="w-full h-full" flexDirection="row" justifyContent="space-between">
-            <label text='{lc('Search.title')}' class="text-center text-lg w-full" />
-        </flexboxLayout>
+    <actionBar title="Search" flat="true" style="padding: 4rem;">
+      <searchBar id="searchbar" row="0" class="text-subheading-md bg-s-light-primary" textFieldBackgroundColor="#F3F4F6" hint="{lc('Search.form.text.hint')}" />  
+        <!-- <flexboxLayout class="w-full h-full" flexDirection="row" justifyContent="space-between"> -->
+          <!-- <label text='{lc('Search.title')}' class="text-center text-heading-sm w-full" /> -->
+        <!-- </flexboxLayout> -->
     </actionBar>
-    
+
   
     <stackLayout
       orientation="vertical"
       horizontalAlignment="center"
       class="w-full"
+      style="border: 0;"
     >
-        <searchBar id="searchbar" row="0" class="bg-none text-lg ml-2 mx-auto" hint="{lc('Search.form.text.hint')}" />
+        <!-- <searchBar id="searchbar" row="0" class="text-subheading-md bg-s-light-primary" textFieldBackgroundColor="#F3F4F6" hint="{lc('Search.form.text.hint')}" /> -->
     
         <label
-            class="w-full h-full text-center align-middle text-lg"
-            text="{lc('Search.results.placeholder')}"
+          style="border: 0;"
+          class="w-full h-full text-center align-middle text-body-md"
+          text="{lc('Search.results.placeholder')}"
         />
     </stackLayout>
   </page>
